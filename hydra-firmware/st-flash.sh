6b55b4fa98581ea09bbced3afcb0c268eb4ac7c4
Muki Kiboigo <muki@kiboigo.com>
2024-04-12 00:11:57 -0700

    Flash Script

--- hydra-firmware/st-flash.sh
+++ hydra-firmware/st-flash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Currently not working.
-# To flash the chip, use GDB load with ST Utils
-st-flash write ./target/thumbv7em-none-eabihf/debug/hydra 0x08000000
+# Build the binary and flashes the chip.
+cargo objcopy --profile flash -- -O binary hydra.bin
+st-flash write ./hydra.bin 0x08000000
